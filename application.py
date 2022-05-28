4f4cd2f6903ba7cdcad607d30fccdbc2cee37f6d
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2022-05-28 23:28:37 +0300

    initial commit

---
+++ application.py
@@ -0,0 +1,13 @@
+"""
+This program detects and reports objects from a picture.
+
+@author: Emir Çetin MEMİŞ
+@contact: memise@mef.edu.tr
+@since: 2020-05-10
+
+@TO-DO:
+    - Implementation of the object detection algorithm.
+    - Creating GUI for the application.
+    - Designing database for the application.
+    - Creating a picture creator for the application.
+"""
