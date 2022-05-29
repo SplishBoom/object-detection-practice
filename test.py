4fcb6f00b1b13a30eabaa3164d6b1d5b62fab052
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2022-05-29 21:01:22 +0300

    data reciving with dicts in test2.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import cv2 as opencv
 import numpy as np
+import datetime
 
 objectNameFilePath      = "Assets/Object Class Names.txt"
 configurationsFilePath  = "Assets/Configurations.pbtxt"
@@ -18,7 +19,9 @@ capModel.setInputScale(1.0/ 127.5)
 capModel.setInputMean((127.5, 127.5, 127.5))
 capModel.setInputSwapRB(True)
 
-while True:
+datas = {}
+
+for i in range(100):
     
     capturedImage = opencv.cvtColor(captureDevice.read()[1],opencv.COLOR_BGR2RGB)
 
@@ -36,8 +39,16 @@ while True:
         i = int(i)
         box = bboxes[i]
         x1,y1,x2,y2 = box[0],box[1],box[2],box[3]
+        
+        objectName = allObjectNames[objectIDs[i]-1].lower()
+        objectStat = "OK"
+        objectCoordinates = (x1,y1,x2,y2)
+        date, time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S").split(" ")
+
         opencv.rectangle(capturedImage, (x1,y1),(x1+x2,y2+y1), color=(0, 255, 0), thickness=2)
-        opencv.putText(capturedImage,allObjectNames[objectIDs[i]-1].upper(),(box[0]+10,box[1]+30), opencv.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+        opencv.putText(capturedImage,objectName.upper(),(box[0]+10,box[1]+30), opencv.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+
+        datas[objectName] = [objectStat,objectCoordinates,date,time]
 
     # show original and processed images in a window togehter
     opencv.imshow("Original",originalImage)
@@ -45,3 +56,5 @@ while True:
     opencv.waitKey(1)
 
 opencv.destroyAllWindows()
+
+print(datas)
---
+++ test2.py
@@ -0,0 +1,6 @@
+import datetime
+
+date, time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S").split(" ")
+
+print(data)
+print(time)
